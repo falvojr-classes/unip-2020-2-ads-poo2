aba387a4b8e705703950daf0de7e83957beb3248
Venilton FalvoJr <falvojr@gmail.com>
2020-09-11 20:47:02 -0300

    Aula 11/09

--- 4. Padroes de Projeto/padroes-de-projeto/src/main/java/br/unip/ads/gof/Program.java
+++ 4. Padroes de Projeto/padroes-de-projeto/src/main/java/br/unip/ads/gof/Program.java
@@ -10,7 +10,7 @@ import br.unip.ads.gof.templatemethod.PacoteA;
 import br.unip.ads.gof.templatemethod.PacoteB;
 import br.unip.ads.gof.templatemethod.Viagem;
 
-public class Main {
+public class Program {
 
 	public static void main(String[] args) {
 		
